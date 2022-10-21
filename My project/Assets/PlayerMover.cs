a3ff1921d4304fee21cf2baecbae068c48629e62
mkjai <mkjaai8964@gmail.com>
2022-10-20 17:05:46 -0700

    day 4

--- My project/Assets/PlayerMover.cs
+++ My project/Assets/PlayerMover.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class PlayerMover : MonoBehaviour
 {
-    float xValue = 0.01f;
-    float yValue = 0.0f;
-    float zValue = 0.0f;
+    [SerializeField] float moveSpeed = 10f;
+
+
     // Start is called before the first frame update
     void Start()
     {
 
     }
 
+
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(xValue, yValue, zValue);
+        MovePlayer();
     }
+
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        transform.Translate(xValue, 0, zValue);
+    }
+
 }
